f2f9833ad4040c41af560c14afc1d031c4ae4936
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-22 19:38:06 +0000

    AM, 3rd commit main

--- src/test/java/com/codeclan/example/employee_tracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/employee_tracker/EmployeeTrackerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.codeclan.example.employee_tracker;
 
+import models.Employee;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import repositeries.EmployeeRepository;
 
 @SpringBootTest
 class EmployeeTrackerApplicationTests {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void canCreateEmployee() {
+		Employee testEmployee = new Employee("test", 30, "123456A", "test@test.com");
+		employeeRepository.save(testEmployee);
+	}
 }
